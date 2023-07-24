344e5f2bc292b605fe75e0127658fd1f91d72573
chizuruIgwe <126483786+chizuruIgwe@users.noreply.github.com>
2023-07-24 13:28:27 +0100

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
-import { FaSearch, } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
